ef2809c4204f9c4cdbc8f3b30e1290407c8466c6
dlwnsgud8406@gmail.com <dlwnsgud8406@gmail.com>
2022-07-28 14:13:45 +0900

    add triangular function

--- 첫번째과제/change.py
+++ 첫번째과제/change.py
@@ -1,15 +1,29 @@
 # from typing import final
 import numpy as np
 import math
+from math import *
+# rotate_arr = np.array(
+#     [
+#         [1, 0, 0],
+#         [0, 0, -1],
+#         [0, 1, 0]
+#     ]
+# )
+x, y, z = input("x, y, z 회전 할 각도를 입력하세요 : ").split()
+
+z = math.radians(int(z))
+y = math.radians(int(y))
+x = math.radians(int(x))
+
 
 rotate_arr = np.array(
     [
-        [1, 0, 0],
-        [0, 0, -1],
-        [0, 1, 0]
+        [cos(z) * cos(y), cos(z) * sin(y) * sin(x) - sin(z) * cos(x), cos(z)*sin(y)*cos(x) + sin(z) * sin(x)],
+        [sin(z) * cos(y), sin(z)*sin(y)*sin(x) + cos(z) * cos(x), sin(z)*sin(y)*cos(x) - cos(z) * sin(x)],
+        [-1*sin(y), cos(y) * sin(x), cos(y) * cos(x)]
     ]
 )
-
+print(rotate_arr)
 file = open('test.obj')
 to_file = open('to.obj', 'w')
 line = file.readline()
@@ -19,7 +33,7 @@ while line :
         to_file.write(line)
 
     line = file.readline()
-    
+
     if 'v ' in line:
         split_line = line.split('v ')[1]
         if ' ' in split_line:
@@ -40,10 +54,10 @@ while line :
             final_str = str(final_array)
             final_str = final_str.replace('[','')
             final_str = final_str.replace(']','')
-            
+
             write_final_str = 'v ' + final_str + '\n'
             write_final_str = write_final_str.replace('  ',' ')
-            print(write_final_str)            
+            print(write_final_str)
             to_file.write(write_final_str)
     else:
         to_file.write(line)
